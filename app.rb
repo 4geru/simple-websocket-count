b137f1b54df423356d7372986f37d66a5454e22e
Rp7rf <westhouse51@gmail.com>
2017-11-30 23:20:39 +0900

    カウントアプリ

---
+++ app.rb
@@ -0,0 +1,47 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader'
+require 'sinatra-websocket'
+require 'json'
+require './models'
+
+
+set :server, 'thin'
+set :sockets, []
+set :counts, []
+
+before do
+  if Count.all.size == 0
+    Count.create(count: 0)
+  end
+end
+
+get '/' do
+  @count = Count.first.count
+  erb :index
+end
+
+get '/websocket/count' do
+  if request.websocket? then
+    request.websocket do |ws|
+      ws.onopen do
+        c = Count.first
+        c.count = c.count + 1
+        c.save
+        settings.sockets << ws
+        puts settings.sockets.size
+        settings.sockets.each do |s| # メッセージを転送
+          s.send(Count.first.count.to_s)
+        end
+      end
+      ws.onmessage do |msg|
+      end
+      ws.onclose do
+        c = Count.first
+        c.count = c.count - 1
+        c.save
+        settings.sockets.delete(ws)
+      end
+    end
+  end
+end
