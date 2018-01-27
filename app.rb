5d26b5adc88ac00f4eb613cad10508e3bce9abb1
Rp7rf <westhouse51@gmail.com>
2018-01-28 01:21:36 +0900

    部屋ごとに石を置けるように変更

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ require './models'
 
 
 set :server, 'thin'
-set :sockets, []
+set :sockets, {}
 
 before do
   Count.create(count: 0) if Count.all.size == 0
@@ -32,46 +32,28 @@ get '/room/:id' do
   erb :room
 end
 
-get '/websocket/count/:id' do
+get '/websocket/count/:id' do |path|
   if request.websocket? then
     request.websocket do |ws|
       ws.onopen do # 接続を開始した時
-        settings.sockets << ws # socketsリストに追加
-        c = Count.first # count の数を増やす
-        c.count += 1
-        c.save
-        settings.sockets.each do |s| # 全体へメッセージを転送
-          puts s.get_peername
-          c = Count.first
-          s.send({type: 'count', count: c.count}.to_json.to_s)
-        end
+        settings.sockets[path] ||= []
+        settings.sockets[path] << ws # socketsリストに追加
       end
       ws.onmessage do |msg| # メッセージを受け取った時
         puts 'メッセージを受け取ったよ！'
         data = JSON.parse(msg)
         case data['type']
-        when 'open', 'close' # 送られたデータが open or close データだったら
-          settings.sockets.each do |s| # 全体へメッセージを転送
-            c = Count.first
-            s.send({type: 'count', count: c.count}.to_json.to_s)
-          end
         when 'board' # 送られたデータが board データだったら
           turn  = data['turn'] == 'black' ? 'white' : 'black'
-          puts data
-          settings.sockets.each do |s| # メッセージを転送
+          settings.sockets[path].each do |s| # メッセージを転送
             s.send({type: 'board', turn: turn, pos: data['pos']}.to_json.to_s)
           end
         end
       end
       ws.onclose do # メッセージを終了する時
-        c = Count.first # count の数を減らす
-        c.count -= 1
-        c.save
-        settings.sockets.each do |s| # 全体へメッセージを転送
-          c = Count.first
-          s.send({type: 'count', count: c.count}.to_json.to_s)
-        end
-        settings.sockets.delete(ws) # socketsリストから削除
+        puts 'onclose'
+        puts path
+        settings.sockets[path].delete(ws) # socketsリストから削除
       end
     end
   end
